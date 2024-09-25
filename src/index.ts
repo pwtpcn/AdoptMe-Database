7a2067b8c73dfeaae126a8dbdbc685ddc4ee27da
Thanapoom.C <palmdz08@gmail.com>
2024-09-26 00:06:00 +0700

    create middleware.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { swagger } from "@elysiajs/swagger";
 import user from "./user";
 import pet from "./pet";
 import adoption from "./adoption";
+import middleware from "./middleware";
 
 const app = new Elysia();
 app.use(
@@ -19,13 +20,6 @@ app.use(
   })
 );
 
-const middleware = new Elysia().onRequest(async ({ set }) => {
-  set.headers["access-control-allow-origin"] = "*";
-  set.headers["access-control-allow-methods"] = "GET, POST, PUT, DELETE, OPTIONS";
-  set.headers["access-control-allow-headers"] = "Content-Type, Authorization";
-  set.status = 204; // No content
-  return null;
-});
 
 app.use(middleware);
 
