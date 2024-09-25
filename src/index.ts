738421ade23593bb25f8cf6e2096a921b19c5f92
Thanapoom.C <palmdz08@gmail.com>
2024-09-25 22:42:48 +0700

    add middleware in index.ts

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,16 @@ app.use(
   })
 );
 
+const middleware = new Elysia().onRequest(async ({ set }) => {
+  set.headers["access-control-allow-origin"] = "*";
+  set.headers["access-control-allow-methods"] = "GET, POST, PUT, DELETE, OPTIONS";
+  set.headers["access-control-allow-headers"] = "Content-Type, Authorization";
+  set.status = 204; // No content
+  return null;
+});
+
+app.use(middleware);
+
 app.use(user);
 app.use(pet);
 app.use(adoption);
