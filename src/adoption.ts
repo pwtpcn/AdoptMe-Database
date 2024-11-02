5f53c76879799920f84779e5a85644d0d0915d2a
Thanapoom.C <palmdz08@gmail.com>
2024-11-03 01:19:17 +0700

    Add: history function, Edit: adopt function

--- src/adoption.ts
+++ src/adoption.ts
@@ -54,8 +54,15 @@ app.post(
   "/post",
   async ({ body }) => {
     try {
+      // Create a date and adjust it to UTC+7 by adding 7 hours (25200 seconds)
+      const addedDate = new Date(Date.now() + 7 * 60 * 60 * 1000).toISOString();
+
       const insertedAdoption = await db.adoption.create({
-        data: body,
+        data: {
+          added_user: body.added_user,
+          pet_id: body.pet_id,
+          added_date: addedDate
+        },
       });
 
       console.log("User inserted successfully: ", insertedAdoption);
@@ -69,13 +76,13 @@ app.post(
     body: t.Object({
       added_user: t.String(),
       pet_id: t.Number(),
-      added_date: t.Date(),
     }),
     detail: {
       tags: ["Adoption"],
     },
   }
 );
+
 
 app.put(
   "/updateAdoptionByID",
@@ -115,12 +122,12 @@ app.put(
   "/adopt",
   async ({ body }) => {
     const adoption: any = await db.$queryRaw`
-      UPDATE adoption 
-         SET adoption_id = (SELECT MAX(adoption_id) + 1 FROM adoption),
-             adopt_user = ${body.adopt_user},
-             adopted_at = ${new Date()}
-         WHERE added_id = ${body.added_id} AND adopt_user IS NULL
-         RETURNING added_id,adoption_id,added_user,adopt_user,pet_id,added_at,adopted_at;
+      UPDATE "adoption" 
+         SET "adoption_id" = (SELECT COUNT("adoption_id") + 1 FROM "adoption"),
+             "adopt_user" = (SELECT "user_id" FROM "user" WHERE "username" = ${body.username}),
+             "adopted_date" = NOW() AT TIME ZONE 'Asia/Bangkok'
+         WHERE "added_id" = ${body.added_id} AND "adopt_user" IS NULL
+         RETURNING "added_id", "adoption_id", "added_user", "adopt_user", "pet_id", "added_date", "adopted_date";
       `;
     if (adoption.length === 0) {
       return {
@@ -145,7 +152,7 @@ app.put(
     },
     body: t.Object({
       added_id: t.Number(),
-      adopt_user: t.Number(),
+      username: t.String(),
     }),
     detail: {
       tags: ["Adoption"],
@@ -182,5 +189,31 @@ app.delete(
     },
   }
 );
+
+app.get(
+  "/history",
+  async () => {
+    const history = await db.$queryRaw`
+      SELECT 
+      "adoption"."added_id",
+      "adoption"."adoption_id",
+      "added_user"."username" AS "added_username",
+      "adoption"."added_date",
+      "adopt_user"."username" AS "adopt_username",
+      "adoption"."adopted_date",
+      "pet"."pet_name"
+      FROM "adoption"
+      LEFT JOIN "user" AS "added_user" ON "added_user"."user_id" = "adoption"."added_user"
+      LEFT JOIN "user" AS "adopt_user" ON "adopt_user"."user_id" = "adoption"."adopt_user"
+      LEFT JOIN "pet" ON "pet"."pet_id" = "adoption"."pet_id";
+    `;
+    return history;
+  },
+  {
+    detail: {
+      tags: ["Adoption"],
+    },
+  }
+)
 
 export default app;
