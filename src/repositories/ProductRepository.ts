81cff3627ddfb4e9fc50edca4fbaaedc4228698f
Kriwn <nonisgod@gmail.com>
2025-03-19 20:20:40 +0700

    Refactor: remove unusefile

---
+++ src/repositories/ProductRepository.ts
@@ -0,0 +1,88 @@
+import { product } from ".prisma/client";
+import db from "./Database";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+
+class ProductRepository {
+
+	public async getAll(): Promise<product[]> {
+		return await db.product.findMany();
+	}
+
+	public async getByName(name: string): Promise<product | null> {
+		return await db.product.findFirst({
+			where: {
+				name: name
+			}
+		});
+	}
+
+	public async createProduct({
+		name,
+		price,
+		stock,
+		description,
+	}: {
+		name: string;
+		price: number;
+		stock: number;
+		description: string;
+	}): Promise<product> {
+		try {
+			const response = await db.product.create({
+				data: {
+					name: name,
+					price: price,
+					stock: stock,
+					description: description,
+				},
+			});
+			return response;
+		} catch (error) {
+			if (error instanceof PrismaClientKnownRequestError) {
+				throw JSON.stringify({error: error.message});
+			}
+			else
+			{
+				throw JSON.stringify({error: 'Unexpected error'});
+			}
+		}
+	}
+
+	public async updateProduct({
+		id,
+		product,
+	}: {
+		id: number;
+		product: Partial<product>;
+	}): Promise<product | null> {
+		try {
+			const response = await db.product.create({
+				data: {
+					name: name,
+					price: price,
+					stock: stock,
+					description: description,
+				},
+			});
+			return response;
+		} catch (error) {
+			if (error instanceof PrismaClientKnownRequestError) {
+				throw JSON.stringify({error: error.message});
+			}
+			else
+			{
+				throw JSON.stringify({error: 'Unexpected error'});
+			}
+		}
+	}
+
+	public async deleteProduct(id: number): Promise<product> {
+		return await db.product.delete({
+			where: { id: id },
+		});
+	}
+
+}
+
+
+export default ProductRepository;
