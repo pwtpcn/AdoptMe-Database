53679dadf55bceb037f63e6598c040807d850eda
Kriwn <nonisgod@gmail.com>
2025-03-21 14:41:27 +0700

    feat(Product):create ProductController

---
+++ src/controllers/ProductController.ts
@@ -0,0 +1,105 @@
+import Elysia, { t } from "elysia";
+import ProductRepository from "../repositories/ProductRepository";
+
+
+const ProductController = new Elysia({
+	prefix: "/api/product",
+	tags: ["Product"],
+})
+
+
+ProductController.get(
+	"/getAll",
+	async () => {
+		const productRepository = new ProductRepository();
+		const product = await productRepository.getAll();
+		return product ?? { error: "Product not found" };
+	},
+	{
+		detail: {
+			summary: "Get all products",
+			description: "Get all products",
+		}
+	}
+)
+
+ProductController.get(
+	"/getById/:id",
+	async ({ params: { id } }) => {
+		const productRepository = new ProductRepository();
+		const product = await productRepository.getById(id);
+		return product ?? { error: "Product not found" };
+	},
+	{
+		params: t.Object({
+			id: t.Number(),
+		}),
+		detail: {
+			summary: "Get product by id",
+			description: "Get product by id",
+		}
+	}
+)
+
+ProductController.post(
+	"/createProduct",
+	async ({ body }) => {
+		const productRepository = new ProductRepository();
+		const product = await productRepository.createProduct(body);
+		return product ?? { error: "Product not created" };
+	},
+	{
+		body: t.Object({
+			name: t.String(),
+			price: t.Number(),
+			stock: t.Number(),
+			description: t.String(),
+			imageurl: t.String(),
+		}),
+		detail: {
+			summary: "Create product",
+			description: "Create product",
+		}
+	}
+)
+
+ProductController.put(
+	"/updateProduct",
+	async ({ body }) => {
+		const productRepository = new ProductRepository();
+		const product = await productRepository.updateProduct({ id:body.id, product: body });
+		return product ?? { error: "Product not updated" };
+	},
+	{
+		body: t.Object({
+			id: t.Number(),
+			name: t.Optional(t.String()),
+			price: t.Optional(t.Number()),
+			stock: t.Optional(t.Number()),
+			description: t.Optional(t.String()),
+			imageurl: t.Optional(t.String()),
+		}),
+		detail: {
+			summary: "Update product",
+			description: "Update product",
+		}
+	}
+)
+
+ProductController.delete(
+	"/deleteProduct",
+	async ({ body }) => {
+		const productRepository = new ProductRepository();
+		const product = await productRepository.deleteProduct(body.id);
+		return product ?? { error: "Product not deleted" };
+	},
+	{
+		body: t.Object({
+			id: t.Number(),
+		}),
+		detail: {
+			summary: "Delete product",
+			description: "Delete product",
+		}
+	}
+)
