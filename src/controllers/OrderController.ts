f66c87945b6501c567fe49ca7e17b8fd00f953b9
Harit Sombatsiri <haritsombatsiri@gmail.com>
2025-03-22 23:34:40 +0700

    ADD : Order repo/controller/schema

---
+++ src/controllers/OrderController.ts
@@ -0,0 +1,99 @@
+import Elysia, { t } from "elysia";
+import OrderRepository from "../repositories/OrderRepository";
+import { order_status } from "@prisma/client";
+
+const OrderController = new Elysia({
+	prefix: "/api/order",
+	tags: ["Order"],
+})
+
+OrderController.get(
+	"/getAll",
+	async () => {
+		const orderRepository = new OrderRepository();
+		const orders = await orderRepository.getAll();
+		return orders ?? { error: "Orders not found" };
+	},
+	{
+		detail: {
+			summary: "Get all orders",
+			description: "Get all orders",
+		}
+	}
+)
+
+OrderController.post(
+	"/getById", 
+	async ({ body: { id } }) => {
+		const orderRepository = new OrderRepository();
+		const order = await orderRepository.getById(id);
+		return order ?? { error: "Order not found" };
+	},
+	{
+		body: t.Object({
+			id: t.Number(),
+		}),
+		detail: {
+			summary: "Get order by id",
+			description: "Get order by id",
+		}
+	}
+)
+
+OrderController.post(
+	"/createOrder",
+	async ({ body : { user_id, product_id, quantity, total_price } }) => {
+		const orderRepository = new OrderRepository();
+		const order = await orderRepository.createOrder({
+            user_id,
+            product_id,
+            quantity,
+            total_price  
+        });
+		return order;
+	},
+	{
+		body: t.Object({
+			user_id: t.String(),
+			product_id: t.Number(),
+			quantity: t.Number(),
+			total_price: t.Number(),
+		}),
+		detail: {
+			summary: "Create order",
+			description: "Create order",
+		}
+	}
+)
+
+OrderController.patch(
+	"/updateOrder",
+	async ({ body : { id, order } }) => {
+		const orderRepository = new OrderRepository();
+		const updatedOrder = await orderRepository.updateOrder({
+            id,
+            order
+		});
+		return updatedOrder;
+	},
+	{
+		body: t.Object({
+			id: t.Number(),
+			order: t.Object({
+				user_id: t.Optional(t.String()),
+				product_id: t.Optional(t.Number()),
+				quantity: t.Optional(t.Number()),
+				total_price: t.Optional(t.Number()),
+				order_status: t.Optional(t.Enum(order_status)),
+				order_date: t.Optional(t.Date())
+			})
+		}),
+		detail: {
+			summary: "Update order",
+			description: "Update order",
+		}
+	}
+)
+
+export default OrderController;
+
---
+++ src/repositories/OrderRepository.ts
@@ -0,0 +1,77 @@
+import { order, order_status, pet } from "@prisma/client";
+import db from "./Database";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+
+class OrderRepository {
+	public async getAll(): Promise<order[]> {
+		return await db.order.findMany();
+	}
+
+	public async getById(id: number): Promise<order | null> {
+		return await db.order.findUnique({
+			where: { id: id }
+		});
+	}
+
+	public async getByUserId(user_id: string): Promise<order[]> {
+		return await db.order.findMany({
+			where: { user_id: user_id }
+		});
+	}
+
+	public async createOrder({
+		user_id,
+		product_id,
+		quantity,
+		total_price
+	}: {
+		user_id: string;
+		product_id: number;
+		quantity: number;
+		total_price: number;
+	}): Promise<order> {
+		try {
+			const response = await db.order.create({
+				data: {
+					user_id: user_id,
+					product_id: product_id,
+					quantity: quantity,
+					total_price: total_price,
+					order_date: new Date()
+				}
+			});
+			return response;
+		} catch (error) {
+			if (error instanceof PrismaClientKnownRequestError) {
+				throw JSON.stringify({error: error.message});
+			}
+			throw JSON.stringify({error: "Internal Server Error"});
+		}
+	}
+
+    public async updateOrder({
+        id,
+        order,
+    }: {
+        id: number;
+        order : Partial<order>;
+    }): Promise<order | null> {
+        try {
+            const response = await db.order.update({
+                where: { id: id },
+                data: { 
+                    ...order
+                }
+            });
+            return response;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                throw JSON.stringify({error: error.message});
+            }
+            throw JSON.stringify({error: "Internal Server Error"});
+        }
+    }
+}
+
+
+export default OrderRepository;
